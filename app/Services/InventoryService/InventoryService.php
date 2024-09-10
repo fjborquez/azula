babc5823f8e090d56945236adcee4195483867b3
Francisco Bórquez <fborquez@outlook.com>
2024-09-09 23:56:50 -0300

    test

--- app/Services/InventoryService/InventoryService.php
+++ app/Services/InventoryService/InventoryService.php
@@ -65,16 +65,18 @@ class InventoryService implements InventoryServiceInterface
         return Inventory::whereHas('productStatus', function ($query) {
             $query->where('is_final_phase', false);
         })
-            ->whereHas('productStatus', function ($query) {
-                $query->wherePivot('product_status_transitions.is_active', true);
-                $query->where(function ($subQuery) {
-                    $subQuery->where('product_status_transitions.product_status_id', 6)
-                        ->whereNotNull('inventories.expiration_date');
+        ->whereHas('productStatus', function ($query) {
+            $query->where('product_status_transitions.is_active', true);
+            $query->where(function ($subQuery) {
+                $subQuery->where(function ($condition) {
+                    $condition->where('product_status_transitions.product_status_id', 6)
+                              ->whereNotNull('inventories.expiration_date');
                 })
-                    ->orWhere('product_status_id', '!=', 6);
-            })
-            ->orderBy('id')
-            ->get();
+                ->orWhere('product_status_transitions.product_status_id', '!=', 6);
+            });
+        })
+        ->orderBy('id')
+        ->get();
     }
 
     public function processInventoryDetailStatusTransitions(): void
