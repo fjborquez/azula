86581b15aa28926325c56ef5c5a6c2c82f0e4422
fjborquez <fjborquez@users.noreply.github.com>
2024-09-10 02:24:15 +0000

    PHP Linting (Pint)

--- app/Services/InventoryService/InventoryService.php
+++ app/Services/InventoryService/InventoryService.php
@@ -65,18 +65,18 @@ class InventoryService implements InventoryServiceInterface
         return Inventory::whereHas('productStatus', function ($query) {
             $query->where('is_final_phase', false);
         })
-        ->whereHas('productStatus', function ($query) {
-            $query->wherePivot('is_active', true);
-            $query->where(function ($subQuery) {
-                $subQuery->where(function ($condition) {
-                    $condition->where('product_status_id', 6)
+            ->whereHas('productStatus', function ($query) {
+                $query->wherePivot('is_active', true);
+                $query->where(function ($subQuery) {
+                    $subQuery->where(function ($condition) {
+                        $condition->where('product_status_id', 6)
                             ->whereNotNull('inventories.expiration_date');
-                })
-                ->orWhere('product_status_id', '!=', 6);
-            });
-        })
-        ->orderBy('id')
-        ->get();
+                    })
+                        ->orWhere('product_status_id', '!=', 6);
+                });
+            })
+            ->orderBy('id')
+            ->get();
     }
 
     public function processInventoryDetailStatusTransitions(): void
