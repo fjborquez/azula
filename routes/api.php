e19e4b5e600785faab0a4dd190c311263af30c5b
Francisco Borquez <fborquez@outlook.com>
2024-12-25 19:09:16 -0300

    discard product endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/inventory', [InventoryController::class, 'store']);
 Route::get('/inventory', [InventoryController::class, 'list']);
 Route::put('/inventory/{id}', [InventoryController::class, 'update']);
+Route::put('/inventory/{id}/discard', [InventoryController::class, 'discard']);
 Route::get('/inventory-not-final-phase', [InventoryController::class, 'listNotFinalPhaseStatus']);
