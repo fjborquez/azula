99e5a3b2443afbca43f91517753389c586cd66a5
Francisco Borquez <fborquez@outlook.com>
2025-01-07 21:49:42 -0300

    test

--- routes/console.php
+++ routes/console.php
@@ -12,4 +12,4 @@ Artisan::command('inspire', function () {
 Schedule::call(function () {
     $inventoryService = app(InventoryService::class);
     $inventoryService->processInventoryDetailStatusTransitions();
-})->dailyAt('00:00');
+})->everyFiveMinutes();
